df725e503548ceba16154c6f567db12bba62608b
aqeel13932 <aqeel.labash@gmail.com>
2017-04-11 14:36:33 +0300

    Add progress bar function, fix bugs

--- output/Test_Functions.py
+++ output/Test_Functions.py
@@ -64,14 +64,14 @@ def plotsteps(eid,x,y,z,strng,vanish):
     font.set_size(15)
     train,test,splitpoint = ReadCSV(eid,vanish)
     ax = plt.subplot(x,y,z+1)
-    ax.text(0,1200,strng,color='green',fontproperties=font)
     ax.set_title('Training model')
     ax.plot(train[0],train[1])
+    ax.text(0,train[1].max()+0.2*train[1].max(),strng,color='green',fontproperties=font)
     #ax.axvspan(train[0]*0.75,train[0].max(),color='red',alpha=0.5)
-    ax.axvspan(0,splitpoint, color='green', alpha=0.8)
-    ax.axvspan(splitpoint,train[0].max() , color='yellow', alpha=0.8)
     ax.set_xlabel('Episode')
     ax.set_ylabel('Steps')
+    ax.axvspan(0,splitpoint, color='green', alpha=0.8)
+    ax.axvspan(splitpoint,train[0].max() , color='yellow', alpha=0.8)
     ax = plt.subplot(x,y,z+2)
     ax.plot(test[0],test[1])
     ax.axvspan(0,splitpoint/10 , color='green', alpha=0.8)
@@ -86,7 +86,7 @@ def plotreward(eid,x,y,z,strng,vanish):
     font.set_size(15)
     train,test,splitpoint = ReadCSV(eid,vanish)
     ax = plt.subplot(x,y,z+1)
-    ax.text(0,1200,strng,color='green',fontproperties=font)
+    ax.text(0,test[2].max()+0.2*test[2].max(),strng,color='green',fontproperties=font)
     ax.set_title('Training model')
     ax.plot(train[0],train[2])
     ax.axvspan(0,splitpoint, color='green', alpha=0.8)
@@ -117,3 +117,59 @@ def Hyperparameters(ax,value,experiements):
     ax.set_xticks(tmp1)
     ax.set_xticklabels(tmp2,rotation=70)
     return ax
+    
+def log_progress(sequence, every=None, size=None, name='Items'):
+    from ipywidgets import IntProgress, HTML, VBox
+    from IPython.display import display
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(sequence)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)     # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, record in enumerate(sequence, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = '{name}: {index} / ?'.format(
+                        name=name,
+                        index=index
+                    )
+                else:
+                    progress.value = index
+                    label.value = u'{name}: {index} / {size}'.format(
+                        name=name,
+                        index=index,
+                        size=size
+                    )
+            yield record
+    except:
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = index
+        label.value = "{name}: {index}".format(
+            name=name,
+            index=str(index or '?')
+        )
